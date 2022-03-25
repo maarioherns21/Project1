2fa4024a820d40768cffb0df79f4c1fabf9b4ae4
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2022-03-24 20:20:16 -0700

    Updating sound

--- intro page/scripts.js
+++ intro page/scripts.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector("button");
 const firstText = [];
-const infoMessage = `THE DEAL
-The deck is divided evenly, with each player receiving 26 cards, dealt one at a time, face down. Anyone may deal first. Each player places their stack of cards face down, in front of them.
+const infoMessage = `THE GOAL
+To be the first player to win all 52 cards
+
+THE DEAL
+The deck is divided evenly, with each player receiving 26 cards, dealt one at a time, face down. Each player places their stack of cards face down, in front of them.
+
 THE PLAY
-Each player turns up a card at the same time and the player with the higher card takes both cards and puts them, face down, on the bottom of his stack.
-If the cards are the same rank, it is War. Each player turns up one card face down and one card face up. The player with the higher cards takes both piles (six cards). If the turned-up cards are again the same rank, each player places another card face down and turns another card face up. The player with the higher card takes all 10 cards, and so on.
+Each player turns up a card at the same time and the player with the higher card takes both cards and puts them, face down, on his stack. If the cards are the same rank, it is War. 
+If the turned-up cards are again the same rank, each player places another card face down and turns another card face up. The player with the higher card takes all, and so on.
+
+HOW TO KEEP SCORE
+The game ends when one player has won all the cards.
 `;
 
 btn.addEventListener("click", function () {
